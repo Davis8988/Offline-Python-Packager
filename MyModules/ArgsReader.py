c3e9c9819e77fc7962abb1b742b73d11182c0baa
davis8988 <Davismarsel@gmail.com>
2020-06-13 11:25:21 +0300

    Finished with app args

--- MyModules/ArgsReader.py
+++ MyModules/ArgsReader.py
@@ -7,37 +7,38 @@ import sys
 
 # Modules
 from MyModules import Configuration
+from MyModules import MyGlobals
 
 parser = argparse.ArgumentParser(description='Offline Exporter & Importer for python packages')
-parser.add_argument("-ep", "--export_pacakges", dest="export", help="Export python packages", action='store_false')
-parser.add_argument("-ip", "--import_pacakges", dest="import", help="Import python packages", action='store_false')
-parser.add_argument("-l", "--log_file", help="File path to write logs to")
+parser.add_argument("-ep", "--export_pacakges", dest="export_packages", help='Export packages that match pattern. Pass "*" for exporting all packages', nargs='?', default=None, const="*")
+parser.add_argument("-et", "--export_to", dest="export_to", help="Export packages to this path", nargs='?', default="${OFFLINE_PYTHON_PACKAGER_HOME}\\exported_packages", const="${OFFLINE_PYTHON_PACKAGER_HOME}\\exported_packages")
+parser.add_argument("-ip", "--import_pacakges", dest="import_packages", help='Import packages that match pattern. Pass "*" for importing all packages', nargs='?', default="*", const="*")
+parser.add_argument("-if", "--import_from", dest="import_from", help='Import python packages from this path', nargs='?', default="${OFFLINE_PYTHON_PACKAGER_HOME}\\exported_packages", const="${OFFLINE_PYTHON_PACKAGER_HOME}\\exported_packages")
+parser.add_argument("-it", "--import_to", dest="import_to", help='Import python packages to this path', nargs='?', default=None, const=None)
+parser.add_argument("-l", "--log_file", help="File path to write logs to", nargs='?', default=None, const=None,)
 parser.add_argument("-v", "--verbose", help="More awsome printings.", action='store_false')
 
 received_args = None
 
 
 def read_args(args_list):
-    log_info("Reading args: {}".format(args_list))
+    log_info("Reading received args: {}".format(args_list))
     global received_args
+
     try:
         received_args = parser.parse_args()
     except SystemExit as errorCode:
-        sys.exit(errorCode)
+        MyGlobals.terminateProgram(errorCode)
     except BaseException as errorMsg:
-        log_error("Error - Failed to parse args: {}\n{}".format(args_list, errorMsg))
-        return False
-    if received_args.conf_file:
-        Configuration.conf_file_path = received_args.conf_file
-    if received_args.verbose:
-        Configuration.is_verbose = received_args.verbose
-    # if parsed_args.tasks_names:
-    #     Configuration.tasks_names_to_execute = get_tasks_names_list(parsed_args.tasks_names)
-    if received_args.log_file:
-        Configuration.log_file = received_args.log_file
+        log_error("Error - Failed to parse received args: {}\n{}".format(args_list, errorMsg))
+        MyGlobals.terminateProgram(1)
+
+    Configuration.is_verbose = received_args.verbose
+    Configuration.export_packages = received_args.export_packages
+    Configuration.import_packages = received_args.import_packages
+    Configuration.log_file = received_args.log_file
 
     log_debug("Finished reading args successfully")
-    return True
 
 
 def log_info(msg=""):
